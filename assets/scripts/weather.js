497d4eb30bdf0f80d73db3a7a9a8ec429401fcaa
rfabreu <raabreugomes@gmail.com>
2023-02-23 17:29:25 -0500

    Updated Weather widget v0.2 - Add new items and glassmorphism effect

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,3 +1,6 @@
+// // DEVELOPED AND IMPLEMENTED BY RAFAEL ABREU
+// CONNECT VIA LINKEDIN AT (https://www.linkedin.com/in/rafael-a-gomes/) OR GITHUB AT (https://github.com/rfabreu)
+
 const weatherFrames = document.querySelectorAll('.weather-frame');
 
 weatherFrames.forEach(frame => {
